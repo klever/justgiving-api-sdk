1fc02f044e80d5d71aa921d4a302d16a878c578a
Robin D'Arcy <robin@klever.co.uk>
2019-11-18 12:29:24 +0000

    Refactor SMS test

--- tests/ResourceClients/SmsTest.php
+++ tests/ResourceClients/SmsTest.php
@@ -27,23 +27,29 @@ class SmsTest extends ResourceClientTestCase
     public function it_updates_the_sms_code_for_a_page()
     {
         $pageShortName = "api-test-" . uniqid();
-        $this->client->fundraising->register(new FundraisingPage([
-            'reference'       => "12345",
-            'pageShortName'   => $pageShortName,
-            'activityType'    => "OtherCelebration",
-            'pageTitle'       => "api test",
-            'pageStory'       => "This is my custom page story, which will override the default.",
-            'eventName'       => "The Other Occasion of ApTest and APITest",
-            'charityId'       => 2050,
-            'targetAmount'    => 20,
-            'eventDate'       => "/Date(1235764800000)/",
-            'charityOptIn'    => true,
-            'charityFunded'   => false,
+        $pageResponse = $this->client->fundraising->register(new FundraisingPage([
+            'reference'     => "12345",
+            'pageShortName' => $pageShortName,
+            'activityType'  => "OtherCelebration",
+            'pageTitle'     => "api test",
+            'pageStory'     => "This is my custom page story, which will override the default.",
+            'eventName'     => "The Other Occasion of ApTest and APITest",
+            'charityId'     => 2050,
+            'targetAmount'  => 20,
+            'eventDate'     => "/Date(1235764800000)/",
+            'charityOptIn'  => true,
+            'charityFunded' => false,
         ]));
+
+        $this->assertSuccessfulResponse($pageResponse);
+
+        $randomChar = function () {
+            return chr(rand(65, 90));
+        };
 
         $payload = new UpdatePageSmsCodeRequest([
             // SMS code must be 4 letters followed by a number between 47 and 99
-            'urn' => chr(rand(65, 90)) . chr(rand(65, 90)) . chr(rand(65, 90)) . chr(rand(65, 90)) . rand(47, 99)
+            'urn' => $randomChar() . $randomChar() . $randomChar() . $randomChar() . rand(47, 99),
         ]);
 
         $response = $this->client->sms->updatePageCode($pageShortName, $payload);
